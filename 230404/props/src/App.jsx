4d3a789f16edb5e0dcecc559dbb0e367fe844c60
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-04 10:09:43 +0900

    230403 카드 복습 완료

--- 230404/props/src/App.jsx
+++ 230404/props/src/App.jsx
@@ -1,9 +1,12 @@
-import Cards from "./components/Cards";
+import Card from "./components/Card";
+import cardData from "./cardData.json";
 
 function App() {
   return (
-    <div className="bg-red-100">
-      <Cards />
+    <div className="grid grid-cols-4 gap-8 py-8 justify-items-center">
+      {cardData.map((v, i) => {
+        return <Card key={i} symbol={v.symbol} score={v.score} />;
+      })}
     </div>
   );
 }
