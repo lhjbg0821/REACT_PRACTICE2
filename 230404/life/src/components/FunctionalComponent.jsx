b337f8f5bd5446a0e3ddd806fd17280c1ae9bd7f
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-04 11:13:01 +0900

    💧useEffect 이용방법..

--- 230404/life/src/components/FunctionalComponent.jsx
+++ 230404/life/src/components/FunctionalComponent.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FunctionComponent = () => {
   const [count, setCount] = useState(0);
+  useEffect(() => {}, []);
 
   return <div className="bg-blue-100 h-96">FunctionComponent</div>;
 };
