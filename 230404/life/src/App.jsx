1dbb791ac1a346b745def12503bd1635da0b7869
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-04 11:46:57 +0900

    Counter

--- 230404/life/src/App.jsx
+++ 230404/life/src/App.jsx
@@ -1,11 +1,23 @@
-import ClassComponent from "./components/ClassComponents";
-import FunctionComponent from "./components/FunctionalComponent";
+import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const onClickToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div className="bg-red-100 min-h-screen">
-      <ClassComponent />
-      <FunctionComponent />
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {toggle && <Counter />}
+      {/* {toggle ? <Counter /> : "카운터"} */}
+      <button
+        onClick={onClickToggle}
+        className="bg-pink-300 px-4 py-2 rounded-xl text-white font-normal"
+      >
+        버튼
+      </button>
     </div>
   );
 }
