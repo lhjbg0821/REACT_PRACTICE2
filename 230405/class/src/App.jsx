36fef4e6612ddcfa8c474ea3402516f579c1ed82
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-05 10:03:36 +0900

    ClassComponent, FunctionalComponent 복습 완료

--- 230405/class/src/App.jsx
+++ 230405/class/src/App.jsx
@@ -1,8 +1,11 @@
 import ClassComp from "./components/ClassComp";
+import Counter from "./components/Counter";
+
 function App() {
   return (
     <div className="bg-red-100">
       <ClassComp />
+      <Counter />
     </div>
   );
 }
--- 230405/class/src/components/ClassComp.jsx
+++ 230405/class/src/components/ClassComp.jsx
@@ -5,8 +5,26 @@ class ClassComp extends Component {
     count: 0,
   };
 
+  setCount(num) {
+    this.setState({
+      count: num,
+    });
+  }
+
   render() {
-    return <div>{this.state.count}</div>;
+    // this는 자기 자신을 가리킴
+    return (
+      <div className="bg-blue-100 w-1/2 min-h-screen flex flex-col justify-center items-center">
+        {this.state.count}
+        <button
+          className="bg-purple-300 px-4 py-2 rounded-md
+        "
+          onClick={() => this.setCount(this.state.count + 1)}
+        >
+          +
+        </button>
+      </div>
+    );
   }
 }
 
