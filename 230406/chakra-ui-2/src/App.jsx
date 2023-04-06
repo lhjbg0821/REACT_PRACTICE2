f159062e612d4eaa261959a194586ffd3c7c2f2a
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-06 10:25:15 +0900

    chakraUI 🍗MenuItem 완료

--- 230406/chakra-ui-2/src/App.jsx
+++ 230406/chakra-ui-2/src/App.jsx
@@ -4,7 +4,10 @@ import {
   Button,
   Menu,
   MenuButton,
-  MenuIcon,
+  MenuItem,
+  MenuList,
+  MenuGroup,
+  MenuDivider,
 } from "@chakra-ui/react";
 import { PhoneIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { useState } from "react";
@@ -22,6 +25,34 @@ function App() {
       >
         Call
       </Button>
+      <Menu>
+        <MenuButton
+          colorScheme="green"
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+        >
+          저녁 메뉴
+        </MenuButton>
+        <MenuList bgColor={"gray.50"}>
+          <MenuGroup title="파스타">
+            <MenuItem>알리오올리오</MenuItem>
+            <MenuItem>토마토파스타</MenuItem>
+            <MenuItem>크림파스타</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="치킨">
+            <MenuItem>양념치킨</MenuItem>
+            <MenuItem>후라이드치킨</MenuItem>
+            <MenuItem>굽네치킨</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="피자">
+            <MenuItem>파인애플 피자</MenuItem>
+            <MenuItem>토마토 피자</MenuItem>
+            <MenuItem>치즈 피자</MenuItem>
+          </MenuGroup>
+        </MenuList>
+      </Menu>
     </ChakraProvider>
   );
 }
