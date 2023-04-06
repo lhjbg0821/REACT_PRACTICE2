3c16ae5bff1b860137bd37857a21a762508b3afd
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-06 10:16:33 +0900

    call 버튼 생성

--- 230406/chakra-ui-2/src/App.jsx
+++ 230406/chakra-ui-2/src/App.jsx
@@ -1,5 +1,29 @@
+import {
+  ChakraProvider,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuIcon,
+} from "@chakra-ui/react";
+import { PhoneIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
 function App() {
-  return <div>Hello, React!</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <ChakraProvider>
+      <Box color="red.400">Hello, Chakra UI~</Box>
+      <Button
+        isLoading={isLoading}
+        colorScheme="pink"
+        leftIcon={<PhoneIcon />}
+        variant={"solid"}
+      >
+        Call
+      </Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
